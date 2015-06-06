494d008184b1e3638b0e351c31dc59efb9725d4c
Phan Hoang Thibaut <thibaut@dream-on.fr>
2015-06-07 01:44:51 +0200

    indentation de la directive : presenter.js

--- src/movies/directives/presenter.js
+++ src/movies/directives/presenter.js
@@ -2,20 +2,22 @@
     'use strict';
 
     angular.module('netMediaApp')
-        .directive('youtube',function($sce){
-  return {
-    restrict: 'EA',
-    scope: { code:'=' },
-    replace: true,
-    template: '<div style="height:400px;"><iframe  width="100%" height="100%" src="{{url}}" frameborder="0" allowfullscreen></iframe></div>',
-    link: function (scope) {
-        console.log('here');
-        scope.$watch('code', function (newVal) {
-           if (newVal) {
-               scope.url = $sce.trustAsResourceUrl("http://www.youtube.com/embed/" + newVal);
-           }
-        });
-    }
-  };
-})
+        .directive('youtube', youtube);
+
+    function youtube($sce){
+      return {
+        restrict: 'EA',
+        scope: { code:'=' },
+        replace: true,
+        template: '<div style="height:400px;"><iframe  width="100%" height="100%" src="{{url}}" frameborder="0" allowfullscreen></iframe></div>',
+        link: function (scope) {
+            console.log('here');
+            scope.$watch('code', function (newVal) {
+               if (newVal) {
+                   scope.url = $sce.trustAsResourceUrl("http://www.youtube.com/embed/" + newVal);
+               }
+            });
+        }
+      };
+    }        
 }());
